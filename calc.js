a099700f05d7d2d306da680d446041c377de7f0d
Cueter0cri <146212739+Cueter0cri@users.noreply.github.com>
2024-09-11 15:29:13 +0400

    calculator

---
+++ calc.js
@@ -0,0 +1,40 @@
+let a = ''; /// first number
+let b = ''; /// second number 
+let sign = ''; /// знак операции
+let finish = false;
+
+const digit = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.' ];
+const action = ['-', '+', 'x', '/']
+
+// экран калькулятора
+
+const out = document.querySelector('.calc-screen p');
+
+function clearAll () {
+    a = ''; // first number and result
+    b = ''; // second number
+    sign = ''; // знак
+    finish = false;
+    out.textContent = 0;
+}
+
+document.querySelector ('.Ac').onclick = clearAll;
+
+document.querySelector ('.buttons').onclick = (event) => {
+    //нажата не кнопка
+    if(!event.target.classList.contains('btn')) return;
+    // нажата кнопка Clear All Ac
+    if(!event.target.classList.contains('Ac')) return;
+
+    out.textContent = ''; 
+    // получаю нажатую кнопку
+    const key = event.target.textContent;
+
+    // если нажата кнопка 0-9 или .
+
+    if (digit.includes(key)) {
+        a+=key;
+        console.log(a, b , sign);
+    }
+}
+
