b1bf8d4e7097f968b667c2129f107caf23616466
Cueter0cri <146212739+Cueter0cri@users.noreply.github.com>
2024-09-12 19:51:41 +0400

    tttt

--- calc.js
+++ calc.js
@@ -3,7 +3,7 @@ let b = ''; /// second number
 let sign = ''; /// знак операции
 let finish = false;
 
-const digit = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.' ];
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.' ];
 const action = ['-', '+', 'x', '/']
 
 // экран калькулятора
@@ -18,13 +18,13 @@ function clearAll () {
     out.textContent = 0;
 }
 
-document.querySelector ('.ac').onclick = clearAll;
+document.querySelector('.ac').onclick = clearAll;
 
-document.querySelector ('.buttons').onclick = (event) => {
+document.querySelector('.buttons').onclick = (event) => {
     //нажата не кнопка
     if(!event.target.classList.contains('btn')) return;
     // нажата кнопка Clear All Ac
-    if(!event.target.classList.contains('ac')) return;
+    if(event.target.classList.contains('ac')) return;
 
     out.textContent = ''; 
     // получаю нажатую кнопку
@@ -33,8 +33,57 @@ document.querySelector ('.buttons').onclick = (event) => {
     // если нажата кнопка 0-9 или .
 
     if (digit.includes(key)) {
-        a+=key;
-        console.log(a, b , sign);
+        if (b === '' && sign === '' ) {
+            a += key;           
+            out.textContent = a;
+        }
+        else if (a!=='' && b!=='' && finish) {
+            b = key;
+            finish = false;
+            out.textContent = b;
+        }
+        else {
+            b += key;
+            out.textContent = b;
+        }
+        console.table(a, b , sign);
+        return;
     }
+
+    // если нажата кнопка  +- *
+
+    if (action.includes(key)) {
+        sign = key;
+        out.textContent = sign;
+        console.table(a, b , sign);
+        return;
+    }
+
+    //нажато =
+    if (key === '=') {
+        switch (sign) {
+            case '+':
+               a = (+a) + (+b);
+               break;
+
+            case '-':
+               a = a - b;
+               break;
+
+            case 'x':
+               a = a * b;
+               break;
+
+            case '/':
+               a = a / b;
+               break;
+        }
+        finish = true;
+        out.textContent = a;
+        console.table(a, b , sign);
+    }
+    
+
+     
 }
 
